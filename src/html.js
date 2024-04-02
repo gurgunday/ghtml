d98ef6fb204ef5f887da20f8c634a84be134c2fd
Gürgün Dayıoğlu <hey@gurgun.day>
2024-04-03 00:24:10 +0200

    initial implementation

--- src/html.js
+++ src/html.js
@@ -53,4 +53,47 @@ const html = (literals, ...expressions) => {
   return accumulator;
 };
 
-export { html };
+/**
+ * @param {{ raw: string[] }} literals
+ * @param {...any} expressions
+ * @yields {string}
+ */
+const htmlGenerator = function* (literals, ...expressions) {
+  const lastLiteralIndex = literals.raw.length - 1;
+
+  if (lastLiteralIndex === -1) {
+    yield "";
+    return;
+  }
+
+  for (let index = 0; index < lastLiteralIndex; ++index) {
+    let literal = literals.raw[index];
+    let expression = expressions[index];
+
+    if (typeof expression?.[Symbol.iterator] === "function") {
+      yield literal;
+      yield* expression;
+    } else {
+      if (typeof expression !== "string") {
+        expression =
+          expression == null
+            ? ""
+            : Array.isArray(expression)
+              ? expression.join("")
+              : `${expression}`;
+      }
+
+      if (literal.length && literal.charCodeAt(literal.length - 1) === 33) {
+        literal = literal.slice(0, -1);
+      } else if (expression.length) {
+        expression = expression.replace(escapeRegExp, escapeFunction);
+      }
+
+      yield literal + expression;
+    }
+  }
+
+  yield literals.raw[lastLiteralIndex];
+};
+
+export { html, htmlGenerator };
